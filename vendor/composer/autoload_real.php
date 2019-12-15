d1d72034742eca4b547ee15927c5a1c312cd6b72
27352 <solidity@gmx.com>
2019-12-14 20:48:34 -0500

    composer update

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -47,6 +47,24 @@ class ComposerAutoloaderInitd12d85afe9f3895d304351fdd9558735
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInitd12d85afe9f3895d304351fdd9558735::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequired12d85afe9f3895d304351fdd9558735($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+function composerRequired12d85afe9f3895d304351fdd9558735($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        require $file;
+
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
     }
 }
